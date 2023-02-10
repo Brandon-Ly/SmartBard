a0fb4578d370810c9257754b50337262cac6fe36
Peter <pc662@drexel.edu>
2023-02-10 18:16:59 -0500

    Fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -19,57 +19,8 @@ import FontContext, {fontNumber} from './SettingFeatures/fonts/font-context.js'
 
 
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import React, {useState} from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-
-
-import Login from './pages/Login.js'
-import Home from './pages/Home.js'
-import Post from './pages/Post.js'
-import Request from './pages/Request.js'
-import RequestCreate from './pages/RequestCreate.js'
-import NavBar from './components/Interface/Navbar'
-import ThemeContext, {themes} from './themes/theme-context'
-import Font from './components/Settings/Font';
-import FontSize from './components/Settings/FontSize';
-import Slider from './components/Settings/Slider';
-
-
-
 
 function App() {
-
-  const [theme, setTheme] = useState(themes.main);
-  const [show, setShow] = useState(false);
-
-  const handleShow = () => setShow(true);
-  const handleClose = () => setShow(false);
-
-
-
-  function handleTheme(color) {
-    switch(color) {
-      case 'main':
-        setTheme(themes.main);
-        localStorage.setItem('theme', JSON.stringify(color));
-        break;
-      case 'purple':
-        setTheme(themes.purple);
-        localStorage.setItem('theme', JSON.stringify(color));
-        break;
-      case 'orange':
-        setTheme(themes.orange);
-        localStorage.setItem('theme', JSON.stringify(color));
-        break;
-      default:
-        break;
-    }
-  }
-
-
 
   const [theme, setTheme] = useState(Themes.main);
   const [show, setShow] = useState(false);
@@ -156,47 +107,7 @@ function App() {
           <Button className='border border-success rounded-circle float-end' size='sm' onClick={handleShow}>Settings</Button>
         </ThemeContext.Provider>
         </FontContext.Provider>
-    
-    <div>
-        <ThemeContext.Provider value={theme}>
-          <NavBar />
-          <Routes>
-            <Route index element={<Login />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/home/post/:postID' element={<Post />} />
-            <Route path='/request' element={<Request />} />
-            <Route path='/create' element={<RequestCreate />} />
-          </Routes>
-
-
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header >
-              <Modal.Title>
-                Settings
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Font />
-              <FontSize />
-              <Slider />
-              <h1>Select Theme</h1>
-              <div>
-                <button className='yellowredButton' onClick={() => handleTheme('main')}></button>
-              </div>
-              <div>
-                <button className='purpleblackButton' onClick={() => handleTheme('purple')}></button>
-              </div>
-              <div>
-                <button className='orangeblueButton' onClick={() => handleTheme('orange')}></button>
-              </div>
-            </Modal.Body>
-          </Modal>
-
-          <Button className='border border-success rounded-circle float-end' size='sm' onClick={handleShow}>Settings</Button>
-        </ThemeContext.Provider>
-    </div>
-    
-    
+        </div>
   );
 }
 
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -27,31 +27,3 @@ export default function Post() {
     </Container>
   )
 }
-import React from 'react'
-import data from "../data.js"
-import {useParams} from 'react-router-dom'
-import {Container, Row, Image, Button} from 'react-bootstrap'
-import "../components/Interface/pages.css"
-
-
-export default function Post() {
-
-    const {postID} = useParams();
-    const post = data.find(postInArray => postInArray.id === parseInt(postID))
-
-    
-
-  return (
-    <Container className="entire-post">
-    <Row><h1 className="text-center" style={{fontSize: 48}} >{post.title}</h1></Row>
-    <Row><p style={{fontSize: 32, padding: 100}}>{post.body}</p></Row>
-    <Row><Image src={post.img} className="d-block mx-auto" style={{
-        maxHeight: '500px', 
-        maxWidth: '50px', 
-        minHeight: '500px', 
-        minWidth: '500px'}} /> </Row>
-    <Row><Button size="lg" variant="warning" style={{marginTop:20}}>Attached Documents</Button></Row>
-    
-    </Container>
-  )
-}
